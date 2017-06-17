6c6771e6dc1ac9484fbe204f615b1c9b25bcbc85
Sihao <1040296932@qq.com>
2017-06-17 17:51:47 +0800

    sopc

---
+++ test/sopc_tb.v
@@ -0,0 +1,23 @@
+`timescale 1ns/1ps
+
+module sopc_tb();
+	reg CLOCK_50;
+	reg rst;
+
+	initial begin
+		CLOCK_50 = 1'b0;
+		forever #10 CLOCK_50 = ~CLOCK_50;
+	end
+
+	initial begin
+		rst = `RstEnable;
+		#195 rst = `RstDisable;
+		#1000 $stop;
+	end
+
+	sopc sopc0(
+		CLOCK_50,
+		rst
+	);
+
+endmodule
